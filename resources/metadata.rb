a0226257e3548df82c3a91998816ed95936e57aa
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2024-05-26 09:52:08 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,7 +1,6 @@
 name             'http2k'
-maintainer       'redborder'
-maintainer_email 'ejimenez@redborder.com'
-license          'All rights reserved'
+maintainer       'Eneo Tecnología S.L.'
+maintainer_email 'git@redborder.com'
+license          'AGPL-3.0'
 description      'Installs/Configures http2k'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.9'
